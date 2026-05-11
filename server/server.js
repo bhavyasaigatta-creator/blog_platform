8b4e93a6ad4b3378d4225d5036ad13a1107ccde5
Bhavya Sai <bhavyasaigatta@gmail.com>
2026-05-12 00:07:29 +0530

    Fixed CORS for Vercel

--- server/server.js
+++ server/server.js
@@ -10,7 +10,11 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173"
+  origin: [
+    "http://localhost:5173",
+    "https://blog-platform-kappa-indol.vercel.app"
+  ],
+  credentials: true
 }));
 app.use(express.json());
 
